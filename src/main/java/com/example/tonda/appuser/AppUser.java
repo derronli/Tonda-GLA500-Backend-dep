a7c68f9aa6cd111c60aed476d6d22bffdbd9da2b
Derron Li <104483680+derronli@users.noreply.github.com>
2023-10-20 23:42:14 -0400

    Refactored AppUser to match new additions

--- src/main/java/com/example/tonda/appuser/AppUser.java
+++ src/main/java/com/example/tonda/appuser/AppUser.java
@@ -28,8 +28,8 @@ public class AppUser implements UserDetails {
     @SequenceGenerator(name="student_sequence", sequenceName = "student_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
@@ -37,14 +37,13 @@ public class AppUser implements UserDetails {
     private Boolean locked;
     private Boolean enabled;
 
-    public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
-        this.name = name;
-        this.username = username;
+    public AppUser(String firstName, String lastName, String email, String password, AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
+
     }
 
     @Override
@@ -58,9 +57,17 @@ public class AppUser implements UserDetails {
         return password;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
     @Override
     public String getUsername() {
-        return username;
+        return email;
     }
 
     @Override
--- src/main/java/com/example/tonda/appuser/AppUserService.java
+++ src/main/java/com/example/tonda/appuser/AppUserService.java
@@ -1,13 +1,35 @@
 package com.example.tonda.appuser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AppUserRepository appUserRepository;
+    private final static String USER_NOT_FOUND = "user with email %s not found";
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+        if (userExists) {
+            throw new IllegalStateException("email taken");
+        }
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        return "login is working";
     }
 }
